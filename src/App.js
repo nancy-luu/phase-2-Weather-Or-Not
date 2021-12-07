7fded11cd6716276601f057e9de93f29a5b5b2cc
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-07 11:06:34 -0800

    weather data change

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ const options = {
 }
 
 
+<<<<<<< HEAD
+=======
+// const lanlngAPI = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=e3c1d63210fbee0969fa2f40280ef636`)
+>>>>>>> 313bc5e5 (useState Weather change)
 // const APIKEY = ("e3c1d63210fbee0969fa2f40280ef636")
 
 
@@ -34,6 +38,12 @@ export default function App(){
     const [zoom, setZoom] = useState(4.3)
     const [weather, setWeather] = useState({})
 
+<<<<<<< HEAD
+=======
+
+    // const cityAPI = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e3c1d63210fbee0969fa2f40280ef636`)
+    
+>>>>>>> 313bc5e5 (useState Weather change)
     const lanlngAPI = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=e3c1d63210fbee0969fa2f40280ef636`)
     
     useEffect(() => {
@@ -60,6 +70,10 @@ export default function App(){
         console.log(zoom)
     }, [lat])
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 313bc5e5 (useState Weather change)
 
   
     const center = {
@@ -96,7 +110,6 @@ export default function App(){
                         city={city}
                         handleCity={handleCity}
                         setCity={setCity}
-                        weather={weather}
                         className="main"/>
                     </Col>
                     <Col className="map">
--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -2,9 +2,14 @@ import React from "react";
 import { Container, Row, Col } from 'react-bootstrap' ;
 
 
+<<<<<<< HEAD
 
 export default function WeatherReport ({weather}){
 
+=======
+export default function WeatherReport (){
+
+>>>>>>> 313bc5e5 (useState Weather change)
     function minmaxTemp(min, max){
         return(
             <h3>
@@ -15,7 +20,6 @@ export default function WeatherReport ({weather}){
     }
     
 
-
     return (
         <>
             <Container className="report">
@@ -23,9 +27,9 @@ export default function WeatherReport ({weather}){
                 <h5 className="weatherIcon">
                     <i className="wi wi-day-sunny display-1"></i>
                 </h5>
-                <h1 className="weatherDegree">25&deg;</h1>
+                <h1 className="weatherDegree"></h1>
                 <h4 className="weatherMaxMin">{minmaxTemp(24, 19)}</h4>
-                <h4 className="weatherDescription">Sunny</h4>
+                <h4 className="weatherDescription"></h4>
                 <button className="favButton" type="submit">Favorite</button>
             </Container>
         </>
