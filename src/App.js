4256139e296a2df8f028c09d16d8eccccf3e2d2a
Jake <jakerfilut@gmail.com>
2021-12-07 09:11:46 -0700

    Fixed weather

--- src/App.js
+++ src/App.js
@@ -2,34 +2,22 @@ import React, {useState, useEffect} from 'react';
 import MapStyles from './MapStyles';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
-
-
-
-
 import { Container, Row, Col } from 'react-bootstrap' ;
-
 import { GoogleMap,
     useLoadScript,
     // Marker,
     // InfoWindow
 } from "@react-google-maps/api";
 
-
-
-// const testAPI =(`https://api.openweathermap.org/data/2.5/weather?q=denver&appid=e3c1d63210fbee0969fa2f40280ef636`)
-
-// const libraries = ["places"]
 const mapContainerStyle = {
     width: '80vh',
     height: '60vh',   
 }
 
-
 const options = {
     styles: MapStyles,
     disableDefaultUI: true,
     zoomControl: true,
-
 }
 
 // const lanlngAPI = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=e3c1d63210fbee0969fa2f40280ef636`)
@@ -38,8 +26,6 @@ const options = {
 // ex: https://api.openweathermap.org/data/2.5/weather?lat=39.7392&lon=104.9903&appid=e3c1d63210fbee0969fa2f40280ef636
 //By city https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-
-
 export default function App(){
 
     const [libraries] = useState(["places"])
@@ -47,16 +33,17 @@ export default function App(){
     const [lng, setLng] = useState(-98.35)
     const [city, setCity] = useState("")
     const [zoom, setZoom] = useState(4.3)
-    // const [weather, setWeather] = useState({})
+    const [weather, setWeather] = useState({})
 
     // const cityAPI = (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=e3c1d63210fbee0969fa2f40280ef636`)
+    
     const lanlngAPI = (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=e3c1d63210fbee0969fa2f40280ef636`)
+    
     useEffect(() => {
         navigator.geolocation.getCurrentPosition((postion) => {
             setLat(postion.coords.latitude)
             setLng(postion.coords.longitude)
             setZoom(11.5)
-            handleCity(lat, lng)
         }
     )
     }, [])
@@ -64,10 +51,15 @@ export default function App(){
     function handleCity({lat,lng}){
         setLng(lng)
         setLat(lat)
+    }
+
+    useEffect(() =>{
         fetch(lanlngAPI)
         .then((res) => res.json())
-        .then((weatherData) => {console.log(weatherData)})
-    }
+        .then((weatherData) => setWeather(weatherData))
+    }, [lat])
+
+    console.log(weather)
 
    
 
@@ -90,11 +82,11 @@ export default function App(){
         <div>
 
             <Header className="header"/>
-            <div class="video-background">
-                <div class="video-foreground">
-                    <iframe src="https://www.youtube.com/embed/Y8ACyHYsb6Q?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist=Y8ACyHYsb6Q" frameborder="0" allowfullscreen allow="autoplay"></iframe>
+            <div className="video-background">
+                <div className="video-foreground">
+                    <iframe src="https://www.youtube.com/embed/Y8ACyHYsb6Q?controls=0&showinfo=0&rel=0&autoplay=1&mute=1&loop=1&playlist=Y8ACyHYsb6Q" frameBorder="0" allowFullScreen allow="autoplay"></iframe>
                 </div>
-                <div class="video-layer"></div>
+                <div className="video-layer"></div>
             </div>
             <Container >
                 <Row >
