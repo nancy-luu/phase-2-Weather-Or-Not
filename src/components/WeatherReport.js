17827a825feef5956b1474694dfe4805c05506ee
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-09 20:57:38 -0800

    Completed Project Pt.2

--- src/components/WeatherReport.js
+++ src/components/WeatherReport.js
@@ -13,25 +13,58 @@ export default function WeatherReport ({ weather , handleSubmit }){
     const weatherId = weather.weather[0].id
     const weatherDescription = weather.weather[0].description
 
+    const fogIcon =
+        <div class="icon cloudy">
+            <div class="cloud"></div>
+            <div class="cloud"></div>
+        </div>
+
+    const snowIcon = 
+        <div class="icon flurries">
+            <div class="cloud"></div>
+            <div class="snow">
+                <div class="flake"></div>
+                <div class="flake"></div>
+            </div>
+        </div>
+
     function getWeatherIcon(weatherId) {
         switch(true){
             case weatherId >= 200 && weatherId <=232:
-                return "wi-thunderstorm";
+                const thunderStormIcon = 
+                    <div class="icon thunder-storm">
+                        <div class="cloud"></div>
+                        <div class="lightning">
+                            <div class="bolt"></div>
+                            <div class="bolt"></div>
+                        </div>
+                    </div>
+                return thunderStormIcon;
             case weatherId >= 300 && weatherId <=321:
-                return "wi-sleet";
+                return snowIcon;
             case weatherId >= 500 && weatherId <=531:
-                console.log("RAIN")
-                return "wi-storm-showers";
+                const rainIcon =
+                    <div class="icon rainy">
+                        <div class="cloud"></div>
+                        <div class="rain"></div>
+                    </div>
+                return rainIcon;
             case weatherId >= 600 && weatherId <=622:
-                return "wi-snow"; 
+                return snowIcon; 
             case weatherId >= 701 && weatherId <=781:
-                return "wi-fog";  
+                return fogIcon;  
             case weatherId === 800:
-                return "wi-day-sunny";
+                const sunnyIcon = 
+                    <div class="icon Sunny">
+                        <div class="sun">
+                            <div class="rays"></div>
+                        </div>
+                    </div>
+                return sunnyIcon;
             case weatherId >= 800 && weatherId <=804:
-                return "wi-day-fog";
+                return fogIcon; 
             default:
-                return "wi-day-fog";
+                return fogIcon; 
         }
     }
     
@@ -40,10 +73,10 @@ export default function WeatherReport ({ weather , handleSubmit }){
             <Container className="report">
                 <h2 className="cityName">{weather.name}</h2>
                 <h5 className="weatherIcon">
-                    <i className={`wi ${getWeatherIcon(weatherId)} display-1`} />
+                    {getWeatherIcon(weatherId)}
                 </h5>
                 <h4 className="weatherDescription">{weatherDescription}</h4>
-                <h1 className="weatherDegree">{Math.round(((weather.main.temp)-273.15)*9/5+32)}&deg;</h1>
+                <h1 className="weatherDegree"> {Math.round(((weather.main.temp)-273.15)*9/5+32)}&deg;</h1>
                 <h4 className="weatherMaxMin">{minmaxTemp(weather.main.temp_max, weather.main.temp_min)}</h4>
                 <h4 className="detailTitle">Details:</h4>
                 <h4 className="detail">Wind Speed: {(weather.wind.speed)} mph</h4>
@@ -58,3 +91,46 @@ export default function WeatherReport ({ weather , handleSubmit }){
     )
 }
 
+{/* <h1>Animated Weather Icons</h1>
+
+<div class="icon sun-shower">
+  <div class="cloud"></div>
+  <div class="sun">
+    <div class="rays"></div>
+  </div>
+  <div class="rain"></div>
+</div>
+
+<div class="icon thunder-storm">
+  <div class="cloud"></div>
+  <div class="lightning">
+    <div class="bolt"></div>
+    <div class="bolt"></div>
+  </div>
+</div>
+
+<div class="icon cloudy">
+  <div class="cloud"></div>
+  <div class="cloud"></div>
+</div>
+
+<div class="icon flurries">
+  <div class="cloud"></div>
+  <div class="snow">
+    <div class="flake"></div>
+    <div class="flake"></div>
+  </div>
+</div>
+
+<div class="icon sunny">
+  <div class="sun">
+    <div class="rays"></div>
+  </div>
+</div>
+
+<div class="icon rainy">
+  <div class="cloud"></div>
+  <div class="rain"></div>
+</div>
+
+<p>Based on <a href="https://dribbble.com/shots/2097042-Widget-Weather" target="_blank">Dribbble</a> by kylor</p> */}
